cce0fd95cf4faf31f42b65b18dfa9ad768a71930
Barkeotullah Opu <146110521+Barkotullah02@users.noreply.github.com>
2025-04-23 20:39:17 +0600

    updated single problem

--- single-post.php
+++ single-post.php
@@ -309,161 +309,94 @@ if (isset($_GET['problemid'])) {
                     </div>
                 </div>
                 <div class="col-lg-4">
-                        <div class="blog_right_sidebar">
-                            <aside class="single_sidebar_widget search_widget">
-                                <div class="input-group">
-                                    <input type="text" class="form-control" placeholder="Search Posts">
-                                    <span class="input-group-btn">
-                                        <button class="btn btn-default" type="button"><i class="ti-search"></i></button>
-                                    </span>
-                                </div><!-- /input-group -->
-                                <div class="br"></div>
-                            </aside>
-                            <aside class="single_sidebar_widget author_widget">
-                                <img class="author_img rounded-circle" src="img/blog/author.png" alt="">
-                                <h4>Charlie Barber</h4>
-                                <p>Senior blog writer</p>
-                                <div class="social_icon">
-                                    <a href="#"><i class="ti-facebook"></i></a>
-                                    <a href="#"><i class="ti-twitter"></i></a>
-                                    <a href="#"><i class="ti-github"></i></a>
-                                    <a href="#"><i class="ti-linkedin"></i></a>
-                                </div>
-                                <p>Boot camps have its supporters andit sdetractors. Some people do not understand why you
-                                    should have to spend money on boot camp when you can get. Boot camps have itssuppor
-                                    ters andits detractors.</p>
-                                <div class="br"></div>
-                            </aside>
-                            <aside class="single_sidebar_widget popular_post_widget">
-                                <h3 class="widget_title">Popular Posts</h3>
+                    <div class="blog_right_sidebar">
+                        <aside class="single_sidebar_widget search_widget">
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Search Posts">
+                                <span class="input-group-btn">
+                                    <button class="btn btn-default" type="button"><i class="ti-search"></i></button>
+                                </span>
+                            </div><!-- /input-group -->
+                            <div class="br"></div>
+                        </aside>
+                        <aside class="single_sidebar_widget popular_post_widget">
+                            <h3 class="widget_title">Popular Posts</h3>
+                            <?php
+                            $popular_problemsaql = "SELECT p.*, COUNT(c.comment_id) AS comment_count
+                                                                        FROM problems p
+                                                                        JOIN comments c ON p.problem_id = c.problem_id
+                                                                        GROUP BY p.problem_id, p.title
+                                                                        ORDER BY comment_count DESC
+                                                                        LIMIT 5;
+                                                                        ";
+                            $popular_problemresult = mysqli_query($connection, $popular_problemsaql);
+                            while ($popularRow = mysqli_fetch_assoc($popular_problemresult)) {
+                                $p_problem_id = $popularRow['problem_id'];
+                                $p_problem_title = $popularRow['title'];
+                                $p_postedate = $popularRow['posted_at'];
+                                $p_problem_image = $popularRow['problem_img'];
+                                ?>
                                 <div class="media post_item">
-                                    <img src="img/blog/popular-post/post1.jpg" alt="post">
+                                    <img style="width: 100px" src="img/problems/<?php echo $p_problem_image; ?>" alt="post">
                                     <div class="media-body">
-                                        <a href="blog-details.html">
-                                            <h3>Space The Final Frontier</h3>
+                                        <a href="single-post.php?problemid=<?php echo $p_problem_id; ?>">
+                                            <h3><?php echo $p_problem_title; ?></h3>
                                         </a>
-                                        <p>02 Hours ago</p>
+                                        <p><?php echo $p_postedate; ?></p>
                                     </div>
                                 </div>
-                                <div class="media post_item">
-                                    <img src="img/blog/popular-post/post2.jpg" alt="post">
-                                    <div class="media-body">
-                                        <a href="blog-details.html">
-                                            <h3>The Amazing Hubble</h3>
-                                        </a>
-                                        <p>02 Hours ago</p>
-                                    </div>
-                                </div>
-                                <div class="media post_item">
-                                    <img src="img/blog/popular-post/post3.jpg" alt="post">
-                                    <div class="media-body">
-                                        <a href="blog-details.html">
-                                            <h3>Astronomy Or Astrology</h3>
-                                        </a>
-                                        <p>03 Hours ago</p>
-                                    </div>
-                                </div>
-                                <div class="media post_item">
-                                    <img src="img/blog/popular-post/post4.jpg" alt="post">
-                                    <div class="media-body">
-                                        <a href="blog-details.html">
-                                            <h3>Asteroids telescope</h3>
-                                        </a>
-                                        <p>01 Hours ago</p>
-                                    </div>
-                                </div>
-                                <div class="br"></div>
-                            </aside>
-                            <aside class="single_sidebar_widget ads_widget">
-                                <a href="#"><img class="img-fluid" src="img/blog/add.jpg" alt=""></a>
-                                <div class="br"></div>
-                            </aside>
-                            <aside class="single_sidebar_widget post_category_widget">
-                                <h4 class="widget_title">Post Catgories</h4>
-                                <ul class="list cat-list">
-                                    <li>
-                                        <a href="#" class="d-flex justify-content-between">
-                                            <p>Technology</p>
-                                            <p>37</p>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="d-flex justify-content-between">
-                                            <p>Lifestyle</p>
-                                            <p>24</p>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="d-flex justify-content-between">
-                                            <p>Fashion</p>
-                                            <p>59</p>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="d-flex justify-content-between">
-                                            <p>Art</p>
-                                            <p>29</p>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="d-flex justify-content-between">
-                                            <p>Food</p>
-                                            <p>15</p>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="d-flex justify-content-between">
-                                            <p>Architecture</p>
-                                            <p>09</p>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="d-flex justify-content-between">
-                                            <p>Adventure</p>
-                                            <p>44</p>
-                                        </a>
-                                    </li>
-                                </ul>
-                                <div class="br"></div>
-                            </aside>
-                            <aside class="single-sidebar-widget newsletter_widget">
-                                <h4 class="widget_title">Newsletter</h4>
-                                <p>
-                                    Here, I focus on a range of items and features that we use in life without
-                                    giving them a second thought.
-                                </p>
-                                <div class="form-group d-flex flex-row">
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                            <div class="input-group-text"><i class="ti-email" aria-hidden="true"></i></div>
-                                        </div>
-                                        <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Enter email"
-                                            onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email'">
-                                    </div>
-                                    <a href="#" class="bbtns">Subcribe</a>
-                                </div>
-                                <p class="text-bottom">You can unsubscribe at any time</p>
-                                <div class="br"></div>
-                            </aside>
-                            <aside class="single-sidebar-widget tag_cloud_widget">
-                                <h4 class="widget_title">Tag Clouds</h4>
-                                <ul class="list">
-                                    <li><a href="#">Technology</a></li>
-                                    <li><a href="#">Fashion</a></li>
-                                    <li><a href="#">Architecture</a></li>
-                                    <li><a href="#">Fashion</a></li>
-                                    <li><a href="#">Food</a></li>
-                                    <li><a href="#">Technology</a></li>
-                                    <li><a href="#">Lifestyle</a></li>
-                                    <li><a href="#">Art</a></li>
-                                    <li><a href="#">Adventure</a></li>
-                                    <li><a href="#">Food</a></li>
-                                    <li><a href="#">Lifestyle</a></li>
-                                    <li><a href="#">Adventure</a></li>
-                                </ul>
-                            </aside>
-                        </div>
+                            <?php } ?>
+                            <div class="br"></div>
+                        </aside>
+                        <aside class="single_sidebar_widget ads_widget">
+                            <a href="#"><img class="img-fluid" src="img/sm-logo.png" alt=""></a>
+                            <div class="br"></div>
+                        </aside>
+                        <aside class="single_sidebar_widget post_category_widget">
+                            <h4 class="widget_title">Post Subjects</h4>
+                            <ul class="list cat-list">
+                                <?php
+                                $get_subject_sql = "SELECT s.name AS name, COUNT(p.problem_id) AS problem_count
+                                                                        FROM problems p
+                                                                        JOIN subjects s ON p.subject_id = s.subject_id
+                                                                        GROUP BY s.subject_id, s.name
+                                                                        ORDER BY problem_count DESC;
+                                                                        ";
+                                $get_subjectresult = mysqli_query($connection, $get_subject_sql);
+                                while ($subjectRow = mysqli_fetch_assoc($get_subjectresult)) {
+                                    $subject_name = $subjectRow['name'];
+                                    $problem_count = $subjectRow['problem_count'];
 
+                                    ?>
+                                    <li>
+                                        <a href="#" class="d-flex justify-content-between">
+                                            <p><?php echo $subject_name; ?></p>
+                                            <p><?php echo $problem_count; ?></p>
+                                        </a>
+                                    </li>
+                                <?php } ?>
+                                <div class="br"></div>
+                        </aside>
+                        <aside class="single-sidebar-widget newsletter_widget">
+                            <h4 class="widget_title">Newsletter</h4>
+                            <p>
+                                Here, I focus on a range of items and features that we use in life without
+                                giving them a second thought.
+                            </p>
+                            <div class="form-group d-flex flex-row">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <div class="input-group-text"><i class="ti-email" aria-hidden="true"></i></div>
+                                    </div>
+                                    <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Enter email"
+                                           onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email'">
+                                </div>
+                                <a href="#" class="bbtns">Subcribe</a>
+                            </div>
+                            <p class="text-bottom">You can unsubscribe at any time</p>
+                            <div class="br"></div>
+                        </aside>
+                    </div>
                 </div>
             </div>
         </div>
